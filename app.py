8e7e1bc8625156a0bfdaca46c72438d203f21edb
Dominicvan2004 <163356198+Dominicvan2004@users.noreply.github.com>
2025-10-05 15:03:07 -0700

    app

--- app.py
+++ app.py
@@ -20,30 +20,14 @@ def home():
     HookedOnSentry.multipage_fetch_NEO_IDs(10)
     
     closests = HookedOnSentry.get_X_soonest_approaching_neo_IDs_from_global_list(10)
-    lsit_of_astroid = []
+    list_of_astroid = []
 
     for i in closests:
-        lsit_of_astroid.append(HookedOnSentry.fetch_asteroid_dictionary(i)['name'])
+        list_of_astroid.append(HookedOnSentry.fetch_asteroid_dictionary(i)['name'])
 
-    return render_template('index.html', items=lsit_of_astroid)
-
-
+    return render_template('index.html', items=list_of_astroid)
 
 
-
-    
-    
-
-#grabs a top ten list of the closets astroids to earth
-
-
-
- 
-
-
-
-
-#A function that returns the asteriods distance in km's from earth
 
 if __name__ == "__main__":
     app.run(debug=True)
